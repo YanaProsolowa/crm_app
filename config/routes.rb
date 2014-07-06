611edd91cbe1d457bb2937189a58de0ca4429194
Yana <janaprosolowa@mail.ru>
2014-07-06 21:42:24 +0700

    Боремся с heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CrmApp::Application.routes.draw do
-  root 'pages#home'
+  root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
